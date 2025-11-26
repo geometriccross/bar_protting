25b76a0f3cd7dc72cd2a86d95d007a8bd74c08a6
geometriccross <geometriccross@desktop.>
2025-11-26 14:56:44 +0900

    add script

--- main.r
+++ main.r
@@ -3,3 +3,18 @@ library(phyloseq)
 library(microViz)
 library(qiime2R)
 
+
+pseq <- qza_to_phyloseq(
+    features = "./q2objs/common_biology_free_table.qza",
+    taxonomy = "./q2objs/common_biology_free_classification.qza",
+    metadata = "./q2objs/metadata.tsv",
+    tree = "./q2objs/biology_free_rooted_tree.qza"
+) %>%
+    tax_fix() %>%
+    tax_fix(unknowns = c("uncultured"))
+
+p <- pseq %>%
+    comp_barplot(tax_level = "Genus") +
+    coord_flip()
+
+ggsave("barplot.svg", plot = p, width = 10, height = 8, dpi = 300)
