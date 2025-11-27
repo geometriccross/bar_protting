8d27fac917fb3b358cc1aa874eb8ab9980cd6e14
geometriccross <geometriccross@desktop.>
2025-11-27 20:00:33 +0900

    ファイルのセットアップを行った

--- src/microbio_plot.r
+++ src/microbio_plot.r
@@ -1,4 +1,13 @@
+library(conflicted)
 library(tidyverse)
 library(phyloseq)
 library(microViz)
-library(qiime2R)
+library(gtools)
+library(dplyr)
+
+source("./src/q2obj.r")
+
+
+conflict_prefer("filter", "dplyr")
+conflict_prefer("lag", "dplyr")
+
