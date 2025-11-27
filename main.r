01a519d3b919dab1b4568cd3265cdfb78fb9d6a7
geometriccross <geometriccross@desktop.>
2025-11-27 15:43:05 +0900

    読込部分と、metadataとの整合を合わせる部分をリファクタリング。関数化し、パイプライン処理で対応

--- main.r
+++ main.r
@@ -4,20 +4,24 @@ library(microViz)
 library(qiime2R)
 
 
-pseq <- qza_to_phyloseq(
+# QIIME2メタデータの読み込みとクリーニング（型定義行の削除とID列の修正）
+clean_sample_data <- read_tsv("./q2objs/metadata.tsv", comment = "") %>%
+    rename_with(~"SampleID", .cols = 1) %>%
+    filter(!str_detect(SampleID, "^#q2:types|^categorical|^numeric")) %>%
+    column_to_rownames("SampleID") %>%
+    sample_data()
+
+# Feature table等の読み込み、分類名の修正、メタデータの結合
+phyloseq_object <- qza_to_phyloseq(
     features = "./q2objs/common_biology_free_table.qza",
     taxonomy = "./q2objs/common_biology_free_classification.qza",
-    metadata = "./q2objs/metadata.tsv",
-    tree = "./q2objs/biology_free_rooted_tree.qza"
+    tree     = "./q2objs/biology_free_rooted_tree.qza"
 ) %>%
     tax_fix() %>%
-    tax_fix(unknowns = c("uncultured"))
+    tax_fix(unknowns = c("uncultured")) %>%
+    merge_phyloseq(clean_sample_data)
 
-metadata <- read_tsv("./q2objs/metadata.tsv", comment = "") %>%
-    rename(SampleID = `#SampleID`) %>%
-    column_to_rownames("SampleID")
 
-sample_data(pseq) <- sample_data(metadata)
 
 print(colnames(sample_data(pseq)))
 
